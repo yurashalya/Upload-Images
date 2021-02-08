95085891d633422c9f39c8d34a25bc3dfb76a07e
YuraS <stallker9696@gmail.com>
2021-02-08 03:36:03 +0200

    Added logic for removing images

--- upload.js
+++ upload.js
@@ -1,4 +1,14 @@
+const bytesToSize = (bytes) => {
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+  if (!bytes) {
+    return "0 Byte";
+  }
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return Math.round(bytes / Math.pow(1024, i)) + " " + sizes[i];
+};
+
 export const uploadFile = (selector, options = {}) => {
+  let files = [];
   const input = document.querySelector(selector);
   const preview = document.createElement("div");
 
@@ -24,7 +34,9 @@ export const uploadFile = (selector, options = {}) => {
       return;
     }
 
-    const files = Array.from(e.target.files);
+    files = Array.from(e.target.files);
+
+    preview.innerHTML = "";
     files.forEach((file) => {
       if (!file.type.match("image")) {
         return;
@@ -37,9 +49,16 @@ export const uploadFile = (selector, options = {}) => {
         preview.insertAdjacentHTML(
           "afterbegin",
           `
-        <div class="preview-image">
-            <img src="${src}" alt="${file.name}"/>
-        </div>
+            <div class="preview-image">
+                <div class="preview-remove" data-name="${
+                  file.name
+                }">&times;</div>
+                <img src="${src}" alt="${file.name}"/>
+                <div class="preview-info">
+                    <span>${file.name}</span>
+                    ${bytesToSize(file.size)}
+                </div>
+            </div>
        `
         );
       };
@@ -47,7 +66,27 @@ export const uploadFile = (selector, options = {}) => {
       reader.readAsDataURL(file);
     });
   };
+  const removeHandler = (e) => {
+    if (!e.target.dataset.name) {
+      return;
+    }
+
+    const { name } = e.target.dataset;
+    files = files.filter((file) => file.name !== name);
+
+    // if (!files.length) {
+    //   upload.style.display = "none";
+    // }
+
+    const block = preview
+      .querySelector(`[data-name="${name}"]`)
+      .closest(".preview-image");
+
+    block.classList.add("removing");
+    setTimeout(() => block.remove(), 300);
+  };
 
   openButton.addEventListener("click", triggerInput);
   input.addEventListener("change", changeHandler);
+  preview.addEventListener("click", removeHandler);
 };
